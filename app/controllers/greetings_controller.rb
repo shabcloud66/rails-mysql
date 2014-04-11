09506a55a45e43d8ce931ba21c344e2bf6e8db78
Philip Kallberg <philip@kallberg.me>
2014-04-11 17:04:34 +0200

    update

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -3,12 +3,7 @@ class GreetingsController < ApplicationController
   caches_page :hello
 
   def hello
-  	@message = "I'm a Rails 4.0.0 Application"
-
-  	ball = Ball.create(:dimples => Random.rand(1000))
-  	@balls = "Created a golf ball with #{ball.dimples} dimples!"
-
-  	@rails_stack_path = ENV['RAILS_STACK_PATH']
+        File.open("/tmp/tja.txt", 'w') { |file| file.write("your text") }
   end
 
   def show
